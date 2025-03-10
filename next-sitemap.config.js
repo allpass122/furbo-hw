43f4a279d5732dc8b4d63c4567440e570efe2ab0
allpass122 <dges5102@gmail.com>
2025-03-11 01:02:34 +0800

    feat: sitemap

---
+++ next-sitemap.config.js
@@ -0,0 +1,7 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+  siteUrl: 'weogje41.vercel.app',
+  generateRobotsTxt: true, // Generate robots.txt file
+  generateIndexSitemap: true, // Generate index-sitemap.xml
+  outDir: 'public',
+};
